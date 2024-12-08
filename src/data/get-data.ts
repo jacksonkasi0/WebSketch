3fb10a211ab631fa02e842b26637bd21ed335e6e
jacksonkasi0 <jacksonkasi@outlook.com>
2024-12-08 21:14:59 +0530

    added my own style

--- src/data/get-data.ts
+++ src/data/get-data.ts
@@ -1,6 +1,6 @@
 import { DocumentData } from "../types";
 
-import page_data from "./pages_example.com_example.com-1732388388557.json"
+import page_data from "./my-example.com.json"
 
 // Function to get JSON data (replace this with actual data fetching logic)
 export async function getJSONData(): Promise<DocumentData> {
